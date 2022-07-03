05371252d66b68cd4601aaf15ace31ff2f949ea3
Ebenezer kiheo <egift8919@gmail.com>
2022-07-03 11:47:56 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 10; digit++)
 	{
-		for (digit2 = 0; digit2 < 10; digit 2++)
+		for (digit2 = 0; digit2 < 10; digit2++)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
