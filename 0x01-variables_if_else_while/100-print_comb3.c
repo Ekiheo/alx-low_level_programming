c596df7160d73a7ecfa668d9afe760dc747c2364
Ebenezer kiheo <egift8919@gmail.com>
2022-07-03 13:17:48 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@ int main(void)
 	int digit1;
 	int digit2;
 
-	for (digit1 = 0; digit1 <= 9; digit1++)
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
 	{
-		for (digit2 = 0; digit2 <= 9; digit2++)
+		for (digit2 = '0'; digit2 <= '9'; digit2++)
 		{
 			if (digit1 < digit2)
 			{
 				putchar(digit1);
 				putchar(digit2);
 
-			if (digit1 != 8 || (digit1 == 8 && digit2 != 9))
+			if (digit1 != '8' || (digit1 == '8' && digit2 != '9'))
 			{
 			putchar(';');
 			putchar(' ');
