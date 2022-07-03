e5e189b2b016adc78f42c6994e5beb4e965775e1
Ebenezer kiheo <egift8919@gmail.com>
2022-07-03 11:45:09 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of two digit numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int digit1;
+	int digit2;
+
+	for (digit1 = 0; digit1 < 10; digit++)
+	{
+		for (digit2 = 0; digit2 < 10; digit 2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (difit1 == 9 && digit2 == 9)
+				continue;
+
+			putchar(';');
+			putcahr(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
