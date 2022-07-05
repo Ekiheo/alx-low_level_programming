e2a9adfd0982d56885b3f47e92e31a9cf1979d9f
Ebenezer kiheo <egift8919@gmail.com>
2022-07-05 01:29:20 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabest in lowercase
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar("%c", a);
+
+	putchar("\n");
+
+	return (0);
+}
