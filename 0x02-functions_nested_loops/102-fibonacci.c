bd3555bcc3934201c5333ee428490ad2b7cd176d
Ebenezer kiheo <egift8919@gmail.com>
2022-07-06 15:24:31 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 fiboacci numbers
+ *
+ * Return: Nothing!
+ */
+int main(void)
+{
+	int 1 = 0;
+	long j = 1, k = 2;
+
+	while (1 < 50)
+	{
+		if (i == 0)
+			printf("%ld", j);
+		else if (i == 1)
+			printf(", %kld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld" k);
+		}
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
