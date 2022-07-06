0ba6d9512499487f10d0a045db154492a2cdcef5
Ebenezer kiheo <egift8919@gmail.com>
2022-07-05 20:42:09 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-	char *s_ = "_putchar";
-
-	while (*s_)
-	{
-		_putchar(*s_);
-		*s_++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
