2e14788933362168932e6d29caae97e9f6db04dd
Ebenezer kiheo <egift8919@gmail.com>
2022-07-05 20:20:23 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * main.h - defines main.h prototypes
- * int _putchar(char);
- * void print_alphabet_x10(void);
- * int _islower(int c);
- * int _isalpha(int c);
- * int print_sign(int n);
- * void jack_bauer(void);
- * void times_table(void);
- * int add(int, int);
- * void print_to_98(int n);
- */
-#endif /** MAIN_H */
+int _putchar(char);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+#endif
