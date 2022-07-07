c7cee51a0c4628c1eebe89261f8f3613f42d4024
Ebenezer kiheo <egift8919@gmail.com>
2022-07-07 05:20:37 -0700

    int _putchar(char);

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0 to 9
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar("%d", a);
+	}
+	_putchar('\n');
+
+	return (0);
+}
