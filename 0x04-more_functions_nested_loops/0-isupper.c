195d6fba3aa9a89152be2b5698de6fcf2269c41a
Ebenezer kiheo <egift8919@gmail.com>
2022-07-07 04:27:06 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c : c - variable
+ *
+ * Return: Returns 1 if c is uppercase else retuns 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
