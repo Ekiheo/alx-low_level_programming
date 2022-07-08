166977ccbcc62f60317f5d13611435e1b1115a97
Ebenezer kiheo <egift8919@gmail.com>
2022-07-07 17:47:41 -0700

    debuging

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests functions that print if integer is positive or negative
+ * Return: 0
+ */
+
+int nain(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
