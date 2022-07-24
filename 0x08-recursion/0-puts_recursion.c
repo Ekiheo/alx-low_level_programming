6cbb8cdd5fb96821ca04d82413509fd1c9ebd7f3
Ebenezer kiheo <egift8919@gmail.com>
2022-07-24 04:18:31 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string input
+ * Return: string followed by a new line
+ */
+void _puts_recursion(char *s)
+{
+	printf("s[]\n");
+}
